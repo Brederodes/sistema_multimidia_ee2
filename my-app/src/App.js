7bbe49588f40b147b92ae2b98769d13e03417473
Miguel Batista <msb2@cesar.school>
2024-11-30 13:44:16 -0300

    Button improvements

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -97,7 +97,7 @@ const App = () => {
               <span
                 style={{
                   color: 'green',
-                  fontSize: '18px',
+                  fontSize: '20px',
                   fontWeight: 'bold',
                   marginRight: '8px',
                 }}
@@ -137,17 +137,6 @@ const App = () => {
               backgroundColor: 'transparent',
             }}
           >
-            {/* WiFi Button with Modal */}
-            <FloatButton
-              icon={<HomeOutlined />}
-              type="primary"
-              style={{
-                right: 165,
-                bottom: 560,
-              }}
-              onClick={() => showModal('WiFi is connected')}
-            />
-
             {/* Sun Button with Modal */}
             <FloatButton
               icon={<SunOutlined />}
@@ -156,7 +145,7 @@ const App = () => {
                 right: 1200,
                 bottom: 175,
               }}
-              onClick={() => showModal('Enjoy the Sun!')}
+              onClick={() => showModal('A energia solar tem um enorme potencial para contribuir com o cumprimento do ODS 11, que visa tornar as cidades mais sustentáveis e resilientes. Ela pode ajudar na criação de infraestruturas urbanas mais ecológicas e na redução das emissões de gases de efeito estufa, fundamentais para mitigar as mudanças climáticas e melhorar a qualidade de vida nas cidades. A implementação de sistemas solares pode diminuir a dependência de fontes de energia não renováveis, aliviar a pressão sobre os sistemas de energia e aumentar a resiliência urbana, especialmente em regiões com alta vulnerabilidade a desastres climáticos. A adoção da energia solar também está alinhada com os esforços globais para promover uma economia mais verde e sustentável, que não apenas melhore a qualidade de vida nas cidades, mas também proporcione uma maior independência energética e redução de impactos ambientais.')}
             />
 
             {/* Thunderbolt Button with Modal */}
@@ -167,7 +156,7 @@ const App = () => {
                 right: 1295,
                 bottom: 645,
               }}
-              onClick={() => showModal('Thunderstorm Warning!')}
+              onClick={() => showModal('A energia eólica tem grande potencial para contribuir com o Objetivo de Desenvolvimento Sustentável (ODS) 11, que visa tornar as cidades mais sustentáveis, resilientes e inclusivas. A geração de energia a partir do vento pode ajudar a reduzir a dependência de fontes de energia poluentes, promover a eficiência energética e garantir a oferta de eletricidade limpa e acessível, características essenciais para a sustentabilidade das cidades. A energia eólica é uma das fontes de energia renovável mais promissoras para mitigar os impactos das mudanças climáticas nas áreas urbanas, uma vez que reduz significativamente as emissões de gases de efeito estufa. Além disso, a implantação de parques eólicos urbanos e em áreas periurbanas pode ajudar a descentralizar a geração de energia, tornando as cidades mais autossuficientes e menos vulneráveis a crises energéticas.')}
             />
 
             {/* Global Button with Modal */}
@@ -178,7 +167,60 @@ const App = () => {
                 right: 1750,
                 bottom: 400,
               }}
-              onClick={() => showModal('Global Information')}
+              onClick={() => showModal('O Objetivo de Desenvolvimento Sustentável (ODS) 11, que busca tornar as cidades e os assentamentos humanos inclusivos, seguros, resilientes e sustentáveis, tem como um de seus eixos a proteção e a gestão sustentável dos recursos naturais. As florestas são um componente essencial nesse processo, pois sua preservação contribui para o equilíbrio climático, a segurança hídrica e a redução de desastres naturais. A ODS 11 busca melhorar a qualidade de vida nas cidades, implementando práticas que integrem a preservação ambiental com o desenvolvimento urbano, promovendo cidades mais verdes e com maior qualidade de vida para seus habitantes​.')}
+            />
+
+            <FloatButton
+              icon={<MenuOutlined />}
+              type="primary"
+              style={{
+                right: 50,
+                bottom: 70,
+              }}
+              onClick={() =>
+                showModal(
+                  <>
+                    <p>
+                      Fontes:
+                      <br />
+                      <a
+                        href="https://www.iberdrola.com/sustentabilidade/comprometidos-objetivos-desenvolvimento-sustentavel/ods-11-cidades-e-comunidades-sustentaveis"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Iberdrola - ODS 11
+                      </a>
+                    </p>
+                    <p>
+                      <a
+                        href="https://123ecos.com.br/docs/ods-11/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        123ecos - ODS 11
+                      </a>
+                    </p>
+                    <p>
+                      <a
+                        href="https://www.edp.com/pt-pt/sustentabilidade/ods-11-cidades-e-comunidades-sustentaveis"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        EDP - ODS 11
+                      </a>
+                    </p>
+                    <p>
+                      <a
+                        href="https://www.unep.org/pt-br/noticias-e-reportagens/comunicado-de-imprensa/os-planos-nacionais-de-acao-climatica-tem-metas"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        UNEP - Os planos nacionais de ação climática têm metas florestais insuficientes e o desmatamento continua a aumentar
+                      </a>
+                    </p>
+                  </>
+                )
+              }
             />
           </Content>
           <Footer
@@ -195,12 +237,12 @@ const App = () => {
 
         {/* Modal Dialog */}
         <Modal
-          title="Information"
+          title="ODS 11 - Cidades Sustentáveis"
           visible={modalVisible}
           onOk={closeModal}
           onCancel={closeModal}
         >
-          <p>{modalContent}</p>
+          <div>{modalContent}</div>
         </Modal>
       </div>
     </>
