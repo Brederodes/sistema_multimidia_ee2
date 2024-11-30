1740020af9da8a834a19336093c6f661620d7fb9
Luiz Felipe Espindola <luizfelipeespindola09@gmail.com>
2024-11-30 16:40:10 -0300

    Replacing buttons and test adding video and audio

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,9 +15,8 @@ import {
   ThunderboltOutlined,
   GlobalOutlined,
   HomeOutlined,
+  MenuOutlined,
 } from '@ant-design/icons';
-
-import { MenuOutlined } from '@ant-design/icons';
 
 const { Header, Content, Footer } = Layout;
 
@@ -142,8 +141,8 @@ const App = () => {
               icon={<SunOutlined />}
               type="primary"
               style={{
-                right: 1200,
-                bottom: 175,
+                right: 1255,
+                bottom: 190,
               }}
               onClick={() => showModal('A energia solar tem um enorme potencial para contribuir com o cumprimento do ODS 11, que visa tornar as cidades mais sustentáveis e resilientes. Ela pode ajudar na criação de infraestruturas urbanas mais ecológicas e na redução das emissões de gases de efeito estufa, fundamentais para mitigar as mudanças climáticas e melhorar a qualidade de vida nas cidades. A implementação de sistemas solares pode diminuir a dependência de fontes de energia não renováveis, aliviar a pressão sobre os sistemas de energia e aumentar a resiliência urbana, especialmente em regiões com alta vulnerabilidade a desastres climáticos. A adoção da energia solar também está alinhada com os esforços globais para promover uma economia mais verde e sustentável, que não apenas melhore a qualidade de vida nas cidades, mas também proporcione uma maior independência energética e redução de impactos ambientais.')}
             />
@@ -153,8 +152,8 @@ const App = () => {
               icon={<ThunderboltOutlined />}
               type="primary"
               style={{
-                right: 1295,
-                bottom: 645,
+                right: 1349,
+                bottom: 668,
               }}
               onClick={() => showModal('A energia eólica tem grande potencial para contribuir com o Objetivo de Desenvolvimento Sustentável (ODS) 11, que visa tornar as cidades mais sustentáveis, resilientes e inclusivas. A geração de energia a partir do vento pode ajudar a reduzir a dependência de fontes de energia poluentes, promover a eficiência energética e garantir a oferta de eletricidade limpa e acessível, características essenciais para a sustentabilidade das cidades. A energia eólica é uma das fontes de energia renovável mais promissoras para mitigar os impactos das mudanças climáticas nas áreas urbanas, uma vez que reduz significativamente as emissões de gases de efeito estufa. Além disso, a implantação de parques eólicos urbanos e em áreas periurbanas pode ajudar a descentralizar a geração de energia, tornando as cidades mais autossuficientes e menos vulneráveis a crises energéticas.')}
             />
@@ -164,10 +163,65 @@ const App = () => {
               icon={<GlobalOutlined />}
               type="primary"
               style={{
-                right: 1750,
-                bottom: 400,
+                right: 1810,
+                bottom: 425,
               }}
-              onClick={() => showModal('O Objetivo de Desenvolvimento Sustentável (ODS) 11, que busca tornar as cidades e os assentamentos humanos inclusivos, seguros, resilientes e sustentáveis, tem como um de seus eixos a proteção e a gestão sustentável dos recursos naturais. As florestas são um componente essencial nesse processo, pois sua preservação contribui para o equilíbrio climático, a segurança hídrica e a redução de desastres naturais. A ODS 11 busca melhorar a qualidade de vida nas cidades, implementando práticas que integrem a preservação ambiental com o desenvolvimento urbano, promovendo cidades mais verdes e com maior qualidade de vida para seus habitantes​.')}
+              onClick={() => showModal(
+                <>
+                  <p>
+                    O Objetivo de Desenvolvimento Sustentável (ODS) 11, que busca tornar as cidades e os assentamentos humanos inclusivos, seguros, resilientes e sustentáveis, tem como um de seus eixos a proteção e a gestão sustentável dos recursos naturais. As florestas são um componente essencial nesse processo, pois sua preservação contribui para o equilíbrio climático, a segurança hídrica e a redução de desastres naturais. A ODS 11 busca melhorar a qualidade de vida nas cidades, implementando práticas que integrem a preservação ambiental com o desenvolvimento urbano, promovendo cidades mais verdes e com maior qualidade de vida para seus habitantes​.
+                  </p>
+                  <p>
+                    Referência:
+                    <br />
+                    <a
+                      href="https://www.unep.org/pt-br/noticias-e-reportagens/comunicado-de-imprensa/os-planos-nacionais-de-acao-climatica-tem-metas"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      UNEP - Os planos nacionais de ação climática têm metas florestais insuficientes e o desmatamento continua a aumentar
+                    </a>
+                  </p>
+                  <div>
+                    <h4>Vídeo Relacionado:</h4>
+                    <video width="560" height="315" controls>
+                      <source src="\my-app\src\videos\ConservacaoFlorestal.mp4" type="video/mp4" />
+                    </video>
+                  </div>
+                  <div>
+                    <audio controls autoPlay>
+                      <source
+                        src="\my-app\src\audios\forest.mp3"
+                        type="audio/mp3"
+                      />
+                    </audio>
+                  </div>
+                </>
+                )
+              }
+            />
+
+            <FloatButton
+              icon={<WifiOutlined />}
+              type="primary"
+              style={{
+                right:170,
+                bottom: 580,
+              }}
+              onClick={() => showModal(
+                  'A energia eólica tem grande potencial para contribuir com o Objetivo de Desenvolvimento Sustentável (ODS) 11, que visa tornar as cidades mais sustentáveis, resilientes e inclusivas. A geração de energia a partir do vento pode ajudar a reduzir a dependência de fontes de energia poluentes, promover a eficiência energética e garantir a oferta de eletricidade limpa e acessível, características essenciais para a sustentabilidade das cidades. A energia eólica é uma das fontes de energia renovável mais promissoras para mitigar os impactos das mudanças climáticas nas áreas urbanas, uma vez que reduz significativamente as emissões de gases de efeito estufa. Além disso, a implantação de parques eólicos urbanos e em áreas periurbanas pode ajudar a descentralizar a geração de energia, tornando as cidades mais autossuficientes e menos vulneráveis a crises energéticas.'
+                )
+              }
+            />
+
+            <FloatButton
+              icon={<HomeOutlined />}
+              type="primary"
+              style={{
+                right:555,
+                bottom:295,
+              }}
+              onClick={() => showModal('A energia eólica tem grande potencial para contribuir com o Objetivo de Desenvolvimento Sustentável (ODS) 11, que visa tornar as cidades mais sustentáveis, resilientes e inclusivas. A geração de energia a partir do vento pode ajudar a reduzir a dependência de fontes de energia poluentes, promover a eficiência energética e garantir a oferta de eletricidade limpa e acessível, características essenciais para a sustentabilidade das cidades. A energia eólica é uma das fontes de energia renovável mais promissoras para mitigar os impactos das mudanças climáticas nas áreas urbanas, uma vez que reduz significativamente as emissões de gases de efeito estufa. Além disso, a implantação de parques eólicos urbanos e em áreas periurbanas pode ajudar a descentralizar a geração de energia, tornando as cidades mais autossuficientes e menos vulneráveis a crises energéticas.')}
             />
 
             <FloatButton
@@ -181,7 +235,7 @@ const App = () => {
                 showModal(
                   <>
                     <p>
-                      Fontes:
+                      Referências:
                       <br />
                       <a
                         href="https://www.iberdrola.com/sustentabilidade/comprometidos-objetivos-desenvolvimento-sustentavel/ods-11-cidades-e-comunidades-sustentaveis"
@@ -209,15 +263,6 @@ const App = () => {
                         EDP - ODS 11
                       </a>
                     </p>
-                    <p>
-                      <a
-                        href="https://www.unep.org/pt-br/noticias-e-reportagens/comunicado-de-imprensa/os-planos-nacionais-de-acao-climatica-tem-metas"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        UNEP - Os planos nacionais de ação climática têm metas florestais insuficientes e o desmatamento continua a aumentar
-                      </a>
-                    </p>
                   </>
                 )
               }
@@ -238,7 +283,7 @@ const App = () => {
         {/* Modal Dialog */}
         <Modal
           title="ODS 11 - Cidades Sustentáveis"
-          visible={modalVisible}
+          open={modalVisible}
           onOk={closeModal}
           onCancel={closeModal}
         >
